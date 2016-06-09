193966396407e5bcf413e2a851681b2b402df723
ghiroe <ghiroe@bu.edu>
2016-06-09 15:06:37 -0400

    button text dynamically changes

--- js/app.js
+++ js/app.js
@@ -2,12 +2,23 @@
   var app = angular.module('appPage', []);
 
   app.controller("DropdownController", function() {
+    this.tab = 0;
+    this.buttonText = functionText[this.tab];
+
     this.selectTab = function(setTab) {
       this.tab = setTab;
+      this.buttonText = functionText[this.tab];
     };
     this.isSelected = function(checkTab) {
       return this.tab === checkTab;
     };
   });
+
+   var functionText = ["Choose a function",
+                "create()",
+                "set()",
+                "get()",
+                "destroy()",
+                "query()"];
 
 })();
