fdd4f8b1c22270298b17b30f4a5271098aa428c9
ghiroe <ghiroe@bu.edu>
2016-06-09 14:41:56 -0400

    migrate ng code to a separate app.js file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-app.controller("DropdownController", function() {
-  this.selectTab = function(setTab) {
-    this.tab = setTab;
-  };
-  this.isSelected = function(checkTab) {
-    return this.tab === checkTab;
-  };
-});
+(function() {
+  var app = angular.module('appPage', []);
+
+  app.controller("DropdownController", function() {
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+})();
