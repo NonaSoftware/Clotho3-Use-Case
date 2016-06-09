96b4345cf7475cf99a311065869c72a3c5016da0
ghiroe <ghiroe@bu.edu>
2016-06-09 12:49:15 -0400

    roll back to functioning page

---
+++ js/app.js
@@ -0,0 +1,8 @@
+app.controller("DropdownController", function() {
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+    return this.tab === checkTab;
+  };
+});
