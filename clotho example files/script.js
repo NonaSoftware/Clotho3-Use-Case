c8f50f5b7f29526dd9d75f9f842731645def9a9a
alexandra-nero <alex@nonasoftware.org>
2016-06-10 13:47:24 -0400

    finished example, made login page

--- clotho example files/script.js
+++ clotho example files/script.js
@@ -3,9 +3,11 @@ function clothoTest(){
 		var password = "alie";
 		Clotho.createUser(username,password).then(function(result){
 			alert(JSON.stringify(result));
+			console.log(result);
 		});
 		Clotho.login(username,password).then(function(result){
 			alert(JSON.stringify(result));
+			console.log(result);
 		});
 		var obj = {};
 		obj["name"] = "example";
@@ -13,7 +15,23 @@ function clothoTest(){
 		obj["schema"] = "org.clothocad.model.sequence";
 		Clotho.create(obj).then(function(result){
 			alert(JSON.stringify(result));
+			console.log(result);
 		});
 
+		var key = "name";
+		var value = "example";
+		var obj2 = {};
+		obj2[key] = value;
+		Clotho.query(obj2).then(function(result){
+			alert(JSON.stringify(result));
+			console.log(result);
+		});
+	}
 
+	function deleting(){
+		var id = document.getElementById("dIdValue").value;
+		Clotho.destroy(id).then(function(result){
+			alert(JSON.stringify(result));
+			console.log(result);
+		});
 	}
