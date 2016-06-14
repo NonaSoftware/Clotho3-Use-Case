0a406db1b50546decf1343347aee24fcf1aab8b6
alexandra-nero <alex@nonasoftware.org>
2016-06-14 11:49:08 -0400

    cleaning up query html

--- js/app.js
+++ js/app.js
@@ -119,46 +119,33 @@
       switch(this.tab){
 
        case 1:
-        if (document.getElementById("map_key1").value!="" && document.getElementById("map_value1").value!=""){
-          var obj = {};
-          obj[document.getElementById("map_key1").value] = document.getElementById("map_value1").value;
-          obj[document.getElementById("map_key2").value] = document.getElementById("map_value2").value;
-          obj[document.getElementById("map_key3").value] = document.getElementById("map_value3").value;
-          Clotho.create(obj).then(function(result){
-      			console.log(result);
-            if (result != null){
-              $('#error_field').text('Object created with ID: ' +result);
-            }
-            else{
-              $('#error_field').text('Cannot create object; connection may be lost.');
-            }
-      		});
-        }
-        else if (document.getElementById("json_object").value!=""){
-            var obj = JSON.parse(document.getElementById("json_object").value);
-            Clotho.create(obj).then(function(result){
-      			console.log(result);
-            if (result != null){
-              $('#error_field').text('Object created with ID: ' +result);
-            }
-            else{
-              $('#error_field').text('Cannot create object; connection may be lost.');
-            }
-      		});
-        }
-        else{
-          $('#error_field').text('Please fill out the appropriate fields.');
-        }
+       //insert getting the key-value pairs here
+       if (document.getElementById("json_object").value!=""){
+         try{
+           var obj = JSON.parse(document.getElementById("json_object").value);
+           Clotho.create(obj).then(function(result){
+             console.log(result);
+             if (result === undefined){
+               $('#error_field').text('Object(s) already exist');
+             }
+             else{
+               var data = JSON.stringify(result, null, 3);
+               $('#error_field').text('Object(s) created with ID: ' + data);
+             }
+           });
+         }
+         catch(err){
+           $('#error_field').text(err.message);
+         }
+       }
+       else{
+         $('#error_field').text('Please fill out the appropriate fields.');
+       }
         break;
 
         case 2:
             try{
               var obj = JSON.parse(document.getElementById("edit_json_object").value);
-            }
-            catch (err){
-              $('#error_field').text(err.message);
-            }
-            try{
               Clotho.set(obj).then(function(result){
                 console.log(result);
                 $('#error_field').text('Object(s) changed.');
@@ -169,40 +156,7 @@
             }
          break;
 
-         case 3:
-          if (document.getElementById("map_key1").value!="" && document.getElementById("map_value1").value!=""){
-            var obj = {};
-            obj[document.getElementById("map_key1").value] = document.getElementById("map_value1").value;
-            obj[document.getElementById("map_key2").value] = document.getElementById("map_value2").value;
-            obj[document.getElementById("map_key3").value] = document.getElementById("map_value3").value;
-            Clotho.query(obj).then(function(result){
-        			console.log(result);
-              if (result != null){
-                var data = JSON.stringify(result);
-                $('#error_field').text('Objects found: ' + data);
-              }
-              else{
-                $('#error_field').text('Cannot find object(s); the object may not exist or connection may be lost');
-              }
-        		});
-          }
-          else if (document.getElementById("json_object").value!=""){
-              var obj = JSON.parse(document.getElementById("json_object").value);
-              Clotho.query(obj).then(function(result){
-        			console.log(result);
-              if (result != null){
-                var data = JSON.stringify(result);
-                $('#error_field').text('Objects found: ' + data);
-              }
-              else{
-                $('#error_field').text('Cannot find object(s); the object may not exist or connection may be lost');
-              }
-        		});
-          }
-          else{
-            $('#error_field').text('Please fill out the appropriate fields.');
-          }
-          break;
+         
        case 4:
           var id = document.getElementById("id_number").value;
      		  Clotho.destroy(id).then(function(result){
